43c58cef09e5057ac228c25857e2154728d34788
thisiszlt <53726478+thisiszlt@users.noreply.github.com>
2019-09-06 17:21:57 +0800

    Revert "Revert "update""
    This reverts commit e767185a3de9f6b2d42f6392038335e02fd55bca.

--- _next/static/X6JKbErO1JKbKdE-hUe7v/pages/components/SettingPage.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{"/F+N":function(n,o,w){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/SettingPage",function(){var n=w("OdG8");return{page:n.default||n}}])}},[["/F+N",1,0]]]);
