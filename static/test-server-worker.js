356817f66fab9e86a90d1869da14ea22b9b65997
thisiszlt <53726478+thisiszlt@users.noreply.github.com>
2019-09-06 17:10:46 +0800

    update

--- static/test-server-worker.js
+++
@@ -1,8 +0,0 @@
-if ('serviceWorker' in navigator) {
-    const swUrl = "./server-worker.js";
-    navigator.serviceWorker
-        .register(swUrl)
-        .then(function () {
-            console.log('Service Worker Registered');
-        });
-}
